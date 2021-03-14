c3c0e97e682ef74b64869f75356f22eb0c3e3295
giulialiotti <giuliana.aliotti@hotmail.com>
2021-03-14 18:05:34 -0300

    Comments on suggestions list

--- js/suggestions-list.js
+++ js/suggestions-list.js
@@ -2,6 +2,7 @@
 // ---- ---- ---- ---- ---- SEARCH GIFOS SUGGESTIONS LIST ---- ---- ---- ---- ----
 // ---- ---- ---- ---- ----  ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---
 
+// Fetch search endpoint with input value 
 async function getSuggestions() {
     let input = document.getElementById('searchInput').value;
     let resp = await fetch(`https://api.giphy.com/v1/gifs/search/tags?&api_key=${APIKEY}&q=${input}&limit=4`);
@@ -10,12 +11,15 @@ async function getSuggestions() {
     return data;
 }
 
+// Draw list under search bar
 function drawSuggestions() {
+  // Empty the list before drawing
   suggestionsList.innerHTML = '';
 
   getSuggestions().then( data => {
     let suggestions = data.data;
 
+    // Looping through the data and creating items
     suggestions.forEach(item => {
       let li = document.createElement('li');
       let span = document.createElement('span');
@@ -28,6 +32,7 @@ function drawSuggestions() {
       li.appendChild(span);
       suggestionsList.appendChild(li);
 
+      // Add event to select each item
       li.addEventListener('click', () => selectItem(item));
 
       //suggestions.name.filter(item => item.toLowerCase().includes(searchInput.value));
@@ -35,11 +40,13 @@ function drawSuggestions() {
   })
 }
 
+// While user is typing show the suggestions list
 searchInput.addEventListener('keyup', () => {
   console.log(searchInput.value);
   drawSuggestions();
 });
 
+// Select each item on the suggestions list and fetch endpoint to show gifs results 
 function selectItem(item) {
   console.log(item.name)
   searchInput.innerText = item.name;
