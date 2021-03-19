d7734ed9ebdade56033af4a4a20bbb28b2c73949
giulialiotti <giuliana.aliotti@hotmail.com>
2021-03-19 18:36:04 -0300

    fetchSearchGifs fuction passed to async, and comments

--- js/search.js
+++ js/search.js
@@ -1,194 +1,166 @@
-// ---- ---- ---- ---- ----  ---- ---- ---- ---- ---- ---- ---- ---- ---- 
+// ---- ---- ---- ---- ----  ---- ---- ---- ---- ---- ---- ---- ---- ----
 // ---- ---- ---- ---- ---- SEARCH GIFOS RESULTS ---- ---- ---- ---- ----
-// ---- ---- ---- ---- ----  ---- ---- ---- ---- ---- ---- ---- ---- ---- 
+// ---- ---- ---- ---- ----  ---- ---- ---- ---- ---- ---- ---- ---- ----
 
 // Hide, hr, trending text, grid and see more button while inactive
 hideSearchInactive();
 
-// btnSearch.addEventListener("click", ev => {
-//     ev.preventDefault();
-//     searchResults();
-// });
-
 function searchResults() {
-    hideTrendingText();
-    
-    // Clean grid search results
-    searchResultsGifs.innerHTML = '';
+  // Hide in desktop and show in mobile trending topics
+  hideTrendingText();
 
-    showSearchActive();
+  // Clean grid search results
+  searchResultsGifs.innerHTML = "";
 
-    let inputText = document.getElementById("searchInput").value;
-    let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&offset=0&q=${inputText}`;
+  // Show grid
+  showSearchActive();
 
-    fetchSearchGifs(url);
+  let inputText = document.getElementById("searchInput").value;
+  let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&offset=0&q=${inputText}`;
 
-    changeTitleSearchResults(inputText);
+  // Fetch searched term and draw results
+  fetchSearchGifs(url).then((gifs) => gifsLoop(gifs));
 
-    // Save input value in local storage and erase it from input
-    localStorage.setItem('inputValue', JSON.stringify(inputText));
-    document.querySelector("#searchInput").value = "";
+  // Show title from search on top of grid
+  changeTitleSearchResults(inputText);
+
+  // Save input value in local storage and erase it from input
+  localStorage.setItem("inputValue", JSON.stringify(inputText));
+  document.querySelector("#searchInput").value = "";
 }
 
 // ---- ---- ---- ---- ---- FETCH API SEARCH GIFS ---- ---- ---- ---- ----
 
-function fetchSearchGifs(url) {
-    fetch(url)
-      .then(response => response.json())
-      .then(content => {
-        let gifs = content.data;
-        gifsLoop(gifs);
-      })
-      .catch(err => {
-        console.error(err);
-      });
+// function fetchSearchGifs(url) {
+//     fetch(url)
+//       .then(response => response.json())
+//       .then(content => {
+//         let gifs = content.data;
+//         gifsLoop(gifs);
+//       })
+//       .catch(err => {
+//         console.error(err);
+//       });
+// }
+
+async function fetchSearchGifs(url) {
+  let response = await fetch(url);
+  let content = await response.json();
+  let gifs = content.data;
+
+  return gifs;
 }
 
-// ---- ---- ---- ---- ---- SEARCH RESULTS GIFS FOR LOOP ---- ---- ---- ---- ----
+// ---- ---- ---- ---- ---- DRAW SEARCH RESULTS GIFS ---- ---- ---- ---- ----
 
 function gifsLoop(gifs) {
-    for (let i = 0; i < gifs.length; i++) {
-        let div = document.createElement("div");
-        let img = document.createElement("img");
+  for (let i = 0; i < gifs.length; i++) {
+    let div = document.createElement("div");
+    let img = document.createElement("img");
 
-        img.src = gifs[i].images.downsized.url;
-        img.alt = gifs[i].title;
+    img.src = gifs[i].images.downsized.url;
+    img.alt = gifs[i].title;
 
-        div.classList.add('gif-container');
+    div.classList.add("gif-container");
 
-        div.addEventListener('mouseenter', () => drawHoverGif(gifs, i, div));
-        div.addEventListener('mouseleave', () => removeHoverGif(div));
-        
-        div.appendChild(img);
-        searchResultsGifs.appendChild(div);
-    }
+    // Hover on gifs event listeners
+    div.addEventListener("mouseenter", () => drawHoverGif(gifs, i, div));
+    div.addEventListener("mouseleave", () => removeHoverGif(div));
+
+    div.appendChild(img);
+    searchResultsGifs.appendChild(div);
+  }
 }
 
 // ---- ---- ---- ---- ---- HIDE AND SHOW ITEMS ---- ---- ---- ---- ----
 
-function showTrendingText() {
+// Hide in desktop and show in mobile trending title and topics
+function hideTrendingText() {
+  if (window.matchMedia("(min-width: 960px)").matches) {
+    /* The screen has at least 960px of width */
+    trendingTitle.style.display = "none";
+    trendingText.style.display = "none";
+  } else {
+    /* The screen has less than 960px of width */
     trendingTitle.style.display = "block";
     trendingText.style.display = "block";
-};
+  }
+}
 
-function hideTrendingText() {
-    if (window.matchMedia("(min-width: 960px)").matches) {
-        /* The screen has at least 960px of width */
-        trendingTitle.style.display = "none";
-        trendingText.style.display = "none";
-    } else {
-        /* The screen has less than 960px of width */
-        trendingTitle.style.display = "block";
-        trendingText.style.display = "block";
-    }
-};
-
+// Hide items while search is inactive
 function hideSearchInactive() {
-    divider.style.display = "none";
-    titleSearchResult.style.display = "none";
-    searchResultsGifs.style.display = "none"; 
-    btnSeeMore.style.display = "none";
-};
+  divider.style.display = "none";
+  titleSearchResult.style.display = "none";
+  searchResultsGifs.style.display = "none";
+  btnSeeMore.style.display = "none";
+}
 
+// Show items while search is active
 function showSearchActive() {
-    divider.style.display = "block";
-    titleSearchResult.style.display = "block";
-    searchResultsGifs.style.display = "grid"; 
-    btnSeeMore.style.display = "flex";
-    btnSeeMore.addEventListener('click', changeOffset);
-};
+  divider.style.display = "block";
+  titleSearchResult.style.display = "block";
+  searchResultsGifs.style.display = "grid";
+  btnSeeMore.style.display = "flex";
+
+  // See more button: when clicked show 12 more gifs
+  btnSeeMore.addEventListener("click", changeOffset);
+}
 
 // ---- ---- ---- ---- ---- SEARCH TITLE ON GRID ---- ---- ---- ---- ----
 
 function changeTitleSearchResults(inputText) {
-    titleSearchResult.innerHTML = inputText;
-};
+  titleSearchResult.innerHTML = inputText;
+}
 
 // ---- ---- ---- ---- ----  ---- ---- ---- ---- ---- ---- ---- ---- ----
 // ---- ---- ---- ---- ---- SEE MORE BUTTON ---- ---- ---- ---- ----
 // ---- ---- ---- ---- ----  ---- ---- ---- ---- ---- ---- ---- ---- ----
 
-function changeOffset() { 
-    offset += 12;
+function changeOffset() {
+  offset += 12;
 
-    let inputText = JSON.parse(localStorage.getItem('inputValue'));
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&offset=${offset}&q=${inputText}`;
-    
-    fetchSearchGifs(url);
+  let inputText = JSON.parse(localStorage.getItem("inputValue"));
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&offset=${offset}&q=${inputText}`;
+
+  fetchSearchGifs(url);
 }
 
 // ---- ---- ---- ---- ----  ---- ---- ---- ---- ---- ---- ---- ---- ---- -
 // ---- ---- ---- ---- ---- TRENDING TEXT IN SEARCH BAR SECTION ---- ---- ---- ---- ----
 // ---- ---- ---- ---- ----  ---- ---- ---- ---- ---- ---- ---- ---- ---- -
 
-// Fetch trending api
-function endPointTrendingGifsText() {
-    fetch(`https://api.giphy.com/v1/trending/searches?api_key=${APIKEY}`)
-    .then(response => response.json())
-    .then(json => {
-        trendingGifsText = json.data;
-        // Create an array of only 5 elements, insert and join in html
-        let arr = trendingGifsText.slice(0, 5);
-        drawTrendingText(arr);
-        eventTrending(arr);
-    })
-    .catch(error => console.error(error));
-};
+// Fetch trending topics
+async function endPointTrendingGifsText() {
+  let response = await fetch(
+    `https://api.giphy.com/v1/trending/searches?api_key=${APIKEY}`
+  );
+  let trendings = await response.json();
 
-endPointTrendingGifsText();
-
-function drawTrendingText(arr) {
-    arr.forEach((trending, i) => {
-        let span = document.createElement('span');
-        let text = trending;
-        if (i < arr.length-1) text += ", "
-        span.textContent = text;
-        trendingText.appendChild(span);  
-    });
-}   
-
-function eventTrending(trending) {
-    trending.addEventListener('click', () => {
-        let urlTrending = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&offset=0&q=${trending}`;
-        console.log('hago click');
-        fetchSearchGifs(urlTrending);
-    });
+  // Return first five elements of the array
+  return trendings.data.slice(0, 5);
 }
- 
 
-//---------------------------------------------------------------------------              
+// Draw trending topics after fetched
+endPointTrendingGifsText().then((trendings) => drawTrendingText(trendings));
 
-// for (let i = 0; i < arr.length; i++) {
-        //     console.log(arr)
-        //     let urlTrending = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&offset=0&q=${arr[i]}`;
-        //     arr[i].addEventListener('click', () => {
-        //         console.log('hago click')
-        //     })
-    //}
+// Draw trending topics
+function drawTrendingText(trendings) {
+  // Loop through trendings array
+  trendings.forEach((trending, i) => {
+    // Create and asign a span for each trendig topic
+    let span = document.createElement("span");
+    let text = trending;
+    span.textContent = text;
+    trendingText.appendChild(span);
 
-// function endPointTrendingGifsText() {
-//     fetch(`https://api.giphy.com/v1/trending/searches?api_key=${APIKEY}`)
-//     .then(response => response.json())
-//     .then(json => {
-//         trendingGifsText = json.data;
+    // Add a coma after first four elements
+    if (i < trendings.length - 1) text += ", ";
 
-//         // Create an array of only 5 elements, insert and join in html
-//         let arr = trendingGifsText.slice(0, 5);
-//         //trendingText.innerHTML = arr.join(', ');
-
-//         arr.forEach(trending => {
-//             let urlTrending = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&offset=0&q=${trending}`;
-//             let aTag = document.createElement('a');
-//             // aTag.href = urlTrending;
-//             aTag.textContent = trending;
-//             trendingText.appendChild(aTag);
-
-//             aTag.onclick = () => { 
-//                 aTag.href = urlTrending;
-//                 fetchSearchGifs(urlTrending)
-//             };
-//         });
-//     })
-//     .catch(error => console.error(error));
-// };
-
+    // When click on a topic fetch item and show results
+    span.addEventListener("click", () => {
+      let urlTrending = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&offset=0&q=${trending}`;
+      fetchSearchGifs(urlTrending).then((gifs) => gifsLoop(gifs));
+      console.log(urlTrending);
+    });
+  });
+}
